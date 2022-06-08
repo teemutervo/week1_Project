d7e8f1733f754a2251a68e8921caaa8daf259f41
teemutervo <103576935+teemutervo@users.noreply.github.com>
2022-06-08 18:09:07 +0300

    Assigments 2.1, 2.2, 2.3 done

--- src/index.js
+++ src/index.js
@@ -14,16 +14,36 @@ addCommentButton.addEventListener("click", function(){
 const addCommentButton = document.getElementById("add-comment");
 const ul = document.getElementById("list");
 addCommentButton.addEventListener("click", () => {
-  const li = document.createElement("li");
+  //const li = document.createElement("li");
+  //const commentValue = document.getElementById("comment-text").value;
+  //li.appendChild(document.createTextNode(commentValue));
+  //ul.appendChild(li);
+
+  const div = document.createElement("div");
   const commentValue = document.getElementById("comment-text").value;
-  li.appendChild(document.createTextNode(commentValue));
-  ul.appendChild(li);
+  const review = document.getElementById("review").value;
+  div.classList.add("comment");
+  commentValue.classList("comment-rating");
+  review.classList.add("comment-tex");
+  div.appendChild(document.createTextNode(review));
+  div.appendChild(document.createTextNode(" " + commentValue));
+  ul.appendChild(div);
 });
 
 function removeComments() {
-  ul.innerHTML = "";
-  confirm("Haluatko varmasti poistaa kommentit?");
+  let question = "Haluatko varmasti poistaa kommentit?";
+  //confirm(question);
+  if (confirm(question) == true) {
+    ul.innerHTML = "";
+  }
 }
+/*addCommentButton.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const commentValue = document.getElementById("comment-text").value;
+  const review = document.getElementById("review").value;
+  div.appendChild(document.createTextNode(commentValue));
+  div.appendChild(document.createTextNode(review));
+});*/
 
 const removeCommentButton = document.getElementById("remove-comments");
 removeCommentButton.addEventListener("click", removeComments);
