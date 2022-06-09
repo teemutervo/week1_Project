46ade231971c0a277fcdd8e12a8017a48c394bb2
teemutervo <103576935+teemutervo@users.noreply.github.com>
2022-06-09 16:31:31 +0300

    assignment 4 done

--- src/index.js
+++ src/index.js
@@ -1,49 +1,40 @@
 import "./styles.css";
 
-/*const addCommentButton = document.getElementById("add-comment");
-
-addCommentButton.addEventListener("click", function(){
-
-    const ul = document.getElementById('ul');
-    const li = document.createElement('li');
-    li.innerHTML = addCommentButton.value;
-    ul.appendChild(li);
-  });
-*/
-
 const addCommentButton = document.getElementById("add-comment");
 const ul = document.getElementById("list");
 addCommentButton.addEventListener("click", () => {
-  //const li = document.createElement("li");
-  //const commentValue = document.getElementById("comment-text").value;
-  //li.appendChild(document.createTextNode(commentValue));
-  //ul.appendChild(li);
-
+  const li = document.createElement("li");
+  const body = document.querySelector("body");
   const div = document.createElement("div");
   const commentValue = document.getElementById("comment-text").value;
   const review = document.getElementById("review").value;
+
+  li.appendChild(document.createTextNode(commentValue));
+  ul.appendChild(li);
+
+  const node1 = document.createTextNode(commentValue);
+  const firstp = document.createElement("p");
+  const node2 = document.createTextNode(review);
+  const secondp = document.createElement("p");
+
+  firstp.appendChild(node1);
+  secondp.appendChild(node2);
+  firstp.classList.add("comment-text");
+  secondp.classList.add("comment-rating");
+
+  div.appendChild(firstp);
+  div.appendChild(secondp);
+
   div.classList.add("comment");
-  commentValue.classList("comment-rating");
-  review.classList.add("comment-tex");
-  div.appendChild(document.createTextNode(review));
-  div.appendChild(document.createTextNode(" " + commentValue));
-  ul.appendChild(div);
+  body.appendChild(div);
 });
 
 function removeComments() {
   let question = "Haluatko varmasti poistaa kommentit?";
-  //confirm(question);
-  if (confirm(question) == true) {
+  if (confirm(question) === true) {
     ul.innerHTML = "";
   }
 }
-/*addCommentButton.addEventListener("click", () => {
-  const div = document.createElement("div");
-  const commentValue = document.getElementById("comment-text").value;
-  const review = document.getElementById("review").value;
-  div.appendChild(document.createTextNode(commentValue));
-  div.appendChild(document.createTextNode(review));
-});*/
 
 const removeCommentButton = document.getElementById("remove-comments");
 removeCommentButton.addEventListener("click", removeComments);
